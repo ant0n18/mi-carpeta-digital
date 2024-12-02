7e55709de8f117b8474d6dc7d8756cfe1d6b0d19
ant0n18 <antonioromero.7827302@gmail.com>
2024-12-02 20:52:05 -0300

    Add files via upload

---
+++ estaticas_y_dinamicas/trabajos_dinamicas/TP1/ejercicio5.php
@@ -0,0 +1,17 @@
+<html>
+
+<?php
+$edad=66;
+
+if($edad>=0 && $edad<=12){
+echo "sos un niño";
+} elseif($edad>=13 && $edad<=17){
+echo"sos un adolecente";
+} elseif($edad>=18 && $edad<=64){
+echo"sos un adulto";
+} elseif ($edad>=65){
+echo"sos un anciano";    
+}
+
+?>
+</hmtl>
---
+++ estaticas_y_dinamicas/trabajos_dinamicas/TP1/ejercicio6.php
@@ -0,0 +1,12 @@
+<html>
+
+<?php
+$num=2;
+
+if($num % 2 == 0){
+    echo"el numero $num es par";
+}else{
+    echo "el numero $num es impar";
+}
+?>
+</hmtl>
